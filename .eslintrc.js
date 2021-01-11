e5909447c8dd8d79007081036ae8ebae736aaf27
Alejandro Serrano <alejandro.serrano@alejandro.serrano>
2021-01-11 13:06:39 -0600

    Add eslint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'node': true
+    },
+    'plugins': ['testcafe'],
+    'extends': ['eslint:recommended', 'plugin:testcafe/recommended'],
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'eqeqeq': 'off',
+        'curly': 'error',
+        'quotes': ['error', 'single']
+    }
+};
